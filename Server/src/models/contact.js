8d598eef48eedbffe5b3b6272cc1300e000cbc7d
Eshan <eshanjain845@gmail.com>
2022-09-19 16:14:53 +0530

    added contact schema

---
+++ Server/src/models/contact.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+mongoose.pluralize(null);
+const Schema = mongoose.Schema;
+
+const contactSchema = new Schema({
+    name:{type:String,required:true},
+    designation:{type:String,required:true},
+    company:{type:String,required:true},
+    industry:{type:String,required:true},
+    email:{type:String,required:true},
+    phoneNumber:{type:Number,required:true},
+    country:{type:String,required:true},
+    userId:{type:Schema.Types.ObjectId,ref:"user"},
+});
+
+const contact = mongoose.model("contact",contactSchema);
+
+module.exports = contact;
