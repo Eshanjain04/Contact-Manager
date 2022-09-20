711f4e9b847da0867e0dd700b5aab800a1b18eb4
raeesmohamed <raeesmohamed66@gmail.com>
2022-09-20 16:16:10 +0530

    registerroute

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const loginRoutes = require("./routers/loginRoutes");
 const contactRoutes = require("./routers/contactsRoutes");
+const registerRoutes = require("./routers/registerRoute")
+
 
 app.use("/login",loginRoutes);
 app.use("/contact",contactRoutes);
-
+app.use("/register",registerRoutes)
 
 module.exports = app;
