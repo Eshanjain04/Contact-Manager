bff4e4223c9acfcc209ac74d3996e5fef803d221
Eshan <eshanjain845@gmail.com>
2022-09-20 17:13:11 +0530

    added login and delete contact routes

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,9 +2,28 @@ const express = require("express");
 const app = express();
 const loginRoutes = require("./routers/loginRoutes");
 const contactRoutes = require("./routers/contactsRoutes");
+const jwt = require("jsonwebtoken");
+const user = require("./models/users");
+require("dotenv").config();
+const secret = process.env.SECRET;
 
 app.use("/login",loginRoutes);
-app.use("/contact",contactRoutes);
 
+app.use("/contact",async (req,res,next)=>{
+    try{
+        const token = req.headers.authorization;
+        const decoded = jwt.verify(token,secret);
+        const userData = await user.findOne({_id:decoded.data});
+        req.user = userData._id;
+        next();
+    }catch(e){
+        res.status(400).json({
+            status:"failed",
+            message:e.message
+        })
+    }
+})
+
+app.use("/contact",contactRoutes);
 
 module.exports = app;
--- Server/src/routers/loginRoutes.js
+++ Server/src/routers/loginRoutes.js
@@ -1,3 +1,41 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+const user = require("../models/users");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const secret = process.env.SECRET;
 
-const router = express.Router()
+router.use(express.json());
+router.use(express.urlencoded({extended:true}));
+
+router.post("/",async(req,res)=>{
+    const mailid = req.body.mailid;
+    const password = req.body.password;
+    const userData = await user.findOne({mailid:mailid});
+    if(userData != null){
+        var result =  await bcrypt.compare(password,userData.password);
+        if(result){
+            const token = jwt.sign({
+                exp: Math.floor(Date.now() / 1000) + (60 * 60),
+                data: userData._id
+              }, secret);
+            res.status(200).json({
+                Status:"Success",
+                token:token
+            })
+        }else{
+            res.status(400).json({
+                status:"failed",
+                message:"password doesnot match"
+            })
+        }
+    }else{
+        res.status(400).json({
+            status:"failed",
+            message:"No user Found"
+        })
+    }
+});
+
+module.exports = router;
