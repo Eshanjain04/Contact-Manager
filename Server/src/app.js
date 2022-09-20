fa1bc828f9c338063c789f28bd94766ad8620ecc
Eshan <eshanjain845@gmail.com>
2022-09-20 14:23:50 +0530

    added routes

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
+const loginRoutes = require("./routers/loginRoutes");
+const contactRoutes = require("./routers/contactsRoutes");
+
+app.use("/login",loginRoutes);
+app.use("/contact",contactRoutes);
+
 
 module.exports = app;
